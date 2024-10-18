1fab695b96474709c5c9bc4ef4c8fe227afd6917
SergiiBartytskyi <s.bartycjkyj@gmail.com>
2024-10-18 21:06:17 +0200

    fix writeSongs

--- src/utils/songs-utils/writeSongs.js
+++ src/utils/songs-utils/writeSongs.js
@@ -2,6 +2,9 @@ import { writeFile } from 'node:fs/promises';
 import { PATH_DB } from '../../songs/songs.js';
 
 export const writeSongs = async (updateData) => {
-  const updateSongs = await writeFile(PATH_DB, JSON.stringify(updateData));
+  const updateSongs = await writeFile(
+    PATH_DB,
+    JSON.stringify(updateData, null, 2),
+  );
   return updateSongs;
 };
