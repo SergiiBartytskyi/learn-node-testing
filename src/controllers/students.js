9024c73d40c352fec2cc398d29a289a4a041d616
SergiiBartytskyi <s.bartycjkyj@gmail.com>
2024-10-22 16:20:46 +0200

    add http-metods post delete put patch

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -1,5 +1,11 @@
 import createHttpError from 'http-errors';
-import { getAllStudents, getStudentById } from '../services/students.js';
+import {
+  createStudent,
+  deleteStudent,
+  getAllStudents,
+  getStudentById,
+  updateStudent,
+} from '../services/students.js';
 
 export const getStudentsController = async (req, res, next) => {
   const students = await getAllStudents();
@@ -23,5 +29,60 @@ export const getStudentByIdController = async (req, res) => {
     status: 200,
     message: `Successfully found student with id ${studentId}!`,
     data: student,
+  });
+};
+
+export const createStudentController = async (req, res) => {
+  const newStudent = await createStudent(req.body);
+
+  res.status(201).json({
+    status: 201,
+    message: 'Successfully created a student!',
+    data: newStudent,
+  });
+};
+
+export const deleteStudentController = async (req, res, next) => {
+  const { studentId } = req.params;
+  const student = await deleteStudent(studentId);
+  if (!student) {
+    next(createHttpError(404, 'Student not found!'));
+    return;
+  }
+
+  res.status(404).send();
+};
+
+export const upsertStudentController = async (req, res, next) => {
+  const { studentId } = req.body;
+  const results = await updateStudent(studentId, req.body, { upsert: true });
+
+  if (!results) {
+    next(createHttpError(404, 'Student not found!'));
+    return;
+  }
+
+  const status = results.isNew ? 201 : 200;
+
+  res.status(status).json({
+    status,
+    message: `Successfully upserted a student!`,
+    data: results.student,
+  });
+};
+
+export const patchStudentController = async (req, res, next) => {
+  const { studentId } = req.params;
+  const result = await updateStudent(studentId, req.body);
+
+  if (!result) {
+    next(createHttpError(404, 'Student not found'));
+    return;
+  }
+
+  res.json({
+    status: 200,
+    message: `Successfully patched a student!`,
+    data: result.student,
   });
 };
